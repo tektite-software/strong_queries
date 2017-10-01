408a23ddde2513b382caef4c0a28af23686765dc
Xavier Bick <fxb9500@gmail.com>
2017-10-01 10:33:38 -0500

    Add some more test cases

--- test/strong_queries/parser_test.rb
+++ test/strong_queries/parser_test.rb
@@ -60,5 +60,23 @@ describe StrongQueries::Parser do
       query_params1.must_equal({test: [:one, :two, :three]})
       query_params2.must_equal({test: [:one, :two, :three]})
     end
+
+    it 'can parse a nested hash' do
+      query = 'filter[where][user_id]=2'
+      rack_parsed_query = Rack::Utils.parse_nested_query(query)
+      params = ActionController::Parameters.new(rack_parsed_query)
+      query_params = params.permit_query_params(filter: { where: [:user_id] } )
+
+      query_params.must_equal({ filter: { where: { user_id: 2 } } } )
+    end
+
+    it 'does not allow unpermitted params' do
+      query = 'filter[order_by]=created_at&filter[pluck]=password_hash'
+      rack_parsed_query = Rack::Utils.parse_nested_query(query)
+      params = ActionController::Parameters.new(rack_parsed_query)
+      query_params = params.permit_query_params(filter: [:order_by] )
+      
+      query_params.must_equal({ filter: { order_by: :created_at } } )
+    end
   end
 end
